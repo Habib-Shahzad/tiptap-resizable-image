6c0a9135fb06148a679b9b29d860177ea112eba2
habib-maqsad <habibshehzad55@gmail.com>
2023-08-23 01:01:01 +0500

    Some Fixes :bug:

--- src/ResizableImage/wrapper/index.tsx
+++ src/ResizableImage/wrapper/index.tsx
@@ -2,37 +2,47 @@ import React from "react";
 import { NodeViewWrapper } from "@tiptap/react";
 import { Resizable } from "re-resizable";
 import { Node as ProsemirrorNode } from "prosemirror-model";
-
+import { Direction } from "re-resizable/lib/resizer";
 interface WrapperProps {
   node: ProsemirrorNode;
   updateAttributes: (attrs: unknown) => void;
 }
 
 export default function ResizableImageWrapper(props: WrapperProps) {
+  const defaultWidth = props.node.attrs.width;
+  const defaultHeight = props.node.attrs.height;
+
+  function getLength(unit: string | undefined) {
+    if (!unit) return 0;
+    return parseInt(unit.split("px")[0]);
+  }
+
   return (
     <NodeViewWrapper className="image-resizer">
       <Resizable
         defaultSize={{
-          width: props.node.attrs.width,
-          height: props.node.attrs.height,
+          width: defaultWidth ? defaultWidth : "300",
+          height: defaultHeight ? defaultHeight : "300",
         }}
         onResize={(
           e: MouseEvent | TouchEvent,
-          direction: any,
+          direction: Direction,
           ref: HTMLElement
         ) => {
+          const updatedWidth = getLength(ref.style.width);
+          const updatedHeight = getLength(ref.style.height);
           props.updateAttributes({
-            width: ref.style.width,
-            height: ref.style.height,
+            width: `${updatedWidth}px`,
+            height: `${updatedHeight}px`,
           });
         }}
         maxWidth={"100%"}
         style={{
           backgroundImage: `url(${props.node.attrs.src})`,
-          backgroundSize: "cover",
+          backgroundSize: "100% 100%",
           backgroundRepeat: "no-repeat",
         }}
-        lockAspectRatio={true}
+        lockAspectRatio={false}
       ></Resizable>
     </NodeViewWrapper>
   );
